f9457753d0dc751ab6648946ca2b3ac9a85d4e0c
Nicole <nicole.franca.goncalves@gmail.com>
2020-07-09 15:21:03 -0300

    Retorno dos links e titulos em um array de objetos

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const fs = require('fs');
+// const fetch = require('node-fetch');
+// const { title } = require('process');
   
   const mdLinks = (path) =>{
-      fs.readFile(path, 'utf8',(error, file)=>{
-        console.log(file);
+    return new Promise((resolve, reject) => {
+      fs.readFile(path, 'utf8',(err, data)=>{
+        const regex = /([^\[\t\n\r]+)\]\(([^#)]+)/g;
+        const arr = [];
+        const arrayLinks = data.match(regex)
+        if (err){
+          reject(err.message);
+        }else{
+        }if (arrayLinks !== null) {
+          let separatingArr = arrayLinks.map((fullArr) => {
+            let arrayPair = fullArr.split(regex);
+            let title = arrayPair.slice(1, -2)[0]
+            let href = arrayPair.slice(2, -1)[0]
+            arr.push({title, href})
+          });
+          resolve(separatingArr);
+          console.log(arr)
+        }else{
+          resolve()
+        }
       });
+    });
   };
 
   mdLinks('./README.md');
